9dd09b97f6e21d508dc2efef6d3e42670bf810f0
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-03-19 18:25:23 +0300

    Добавил третье задание 2 лаб. работы

---
+++ lab2/remove_duplicates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Console.Write("Incorrect number of arguments! " + "\n" + "Usage remove_duplicates.exe <input string>");
+            }
+            else
+            {
+                string transmittedString = args[0];
+                int index = 0;
+                for (int i = 0; i < transmittedString.Length; i++)
+                {
+                    index = transmittedString.IndexOf(transmittedString[i], i + 1);
+
+                    while (index != -1)
+                    {
+                        transmittedString = transmittedString.Remove(index, 1);
+                        index = transmittedString.IndexOf(transmittedString[i], i + 1);
+                    }
+                }
+
+                Console.Write(transmittedString);
+            }
+        }
+    }
+}
