e2855309fef476240bdf1e460ba3766cc7400f5c
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-03-19 18:24:57 +0300

    Добавил второе задание 2 лаб. работы

---
+++ lab2/print_args/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace print_args
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = args.Length;
+
+            if (amount == 0)
+            {
+                Console.Write("No parameters were specified!");
+            }
+            else
+            {
+                Console.Write("Number of arguments: " + amount + '\n');
+                Console.Write("Arguments: ");
+                for (int i = 0; i < amount; i++)
+                {
+                    Console.Write(args[i] + ' ');
+                }
+            }
+        }
+    }
+}
